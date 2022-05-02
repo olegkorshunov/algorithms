13bbac616f10c865876078bdb7a0e3d4b21ace45
Oleg Korshunov <oleg.korshunov3000@gmail.com>
2022-05-02 16:20:15 +0300

    init solution

---
+++ stack_tasks/daily_temperatures/solution.py
@@ -0,0 +1,10 @@
+from stack.stack import Stack
+from typing import List
+
+
+class Solution:
+    def __init__(self):
+        self.stack = Stack()
+
+    def __call__(self, temperature: List[int]) -> List[int]:
+        pass

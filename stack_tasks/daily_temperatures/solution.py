4683dfa37f0b64e06985e9b26f3fbbb568f96775
Oleg Korshunov <oleg.korshunov3000@gmail.com>
2022-05-02 22:32:59 +0300

    solution

--- stack_tasks/daily_temperatures/solution.py
+++ stack_tasks/daily_temperatures/solution.py
@@ -2,9 +2,28 @@ from stack.stack import Stack
 from typing import List
 
 
-class Solution:
+class DailyTemperature:
     def __init__(self):
         self.stack = Stack()
 
     def __call__(self, temperature: List[int]) -> List[int]:
-        pass
+
+        result = []
+        for i in enumerate(temperature[::-1]):
+            if self.stack.isEmpty():
+                if self.stack.isEmpty():
+                    self.stack.push(i)
+                    result.append(0)
+                elif i[1] <= self.stack.peek()[1]:
+                    self.stack.push(i)
+                    result.append(1)
+
+            while i[1] >= self.stack.peek()[1]:
+                self.stack.pop()
+                if self.stack.isEmpty():
+                    self.stack.push(i)
+                    result.append(0)
+                    break
+            else:
+                result.append(i[0] - self.stack.peek()[0])
+                self.stack.push(i)
